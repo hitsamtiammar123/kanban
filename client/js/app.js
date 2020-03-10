410c41007fc80a8a1fdcd01dab5fd168f8452a9b
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-03-10 19:40:21 +0700

    - Penambahan style untuk halaman login di style.css - load source vue.js - Memasukan tiap view html dari masing2 komponen ke tag template - Penamnbahan tombol profile di samping tombol logout - Mulai untuk implementasi vue.js untuk halaman login dan kanban - Ujicoba penggunaan vue.js di halaman kanban untuk action add task, edit, dan delete - Penggunaan partial view dengan ajax tidak akan dilakukan karena masing2 view sudah dapat di-load dari tag template walaupun ini masih belum best practice

--- client/js/app.js
+++ client/js/app.js
@@ -1,35 +1,121 @@
 (function(){
     window.TOKEN=localStorage.getItem('token');
 
-    var $app=$('#app');
-    var pageCache={};
-
-    function onPageLoaded(page){
-        return function(data){
-            $app.html(data);
-            pageCache[page]=data;
+    Vue.component('navbar',{
+        template:'#navbar',
+        methods:{
         }
-    }
+    })
 
-    function onPageFail(jqXHR,status,res){
-        console.log({jqXHR,status,res});
-    }
+    Vue.component('login',{
+        template:'#login-page'
+    });
 
-    function loadPage(page){
-        if(pageCache[page]){
-            $app.html(pageCache[page])
+    Vue.component('kanban',{
+        template:'#kanban-page',
+        data:function(){
+            return {
+                listKanban:[
+                    {
+                        title:'Ini Judul Heheheh',
+                        tasks:[
+                            {
+                                id:1,
+                                task:'Hahahahaahahahah'
+                            },
+                            {
+                                id:2,
+                                task:'Heheheh HOhoohoho'
+                            },
+                            {
+                                id:3,
+                                task:'Hihihi Hahahah Heheheheh'
+                            },
+                        ]
+                    },
+                    {
+                        title:'Ini Judul Heheheh 55555',
+                        tasks:[
+                            {
+                                id:1,
+                                task:'Hahahahaahahahah'
+                            },
+                            {
+                                id:2,
+                                task:'Heheheh HOhoohoho'
+                            },
+                            {
+                                id:3,
+                                task:'Hihihi Hahahah Heheheheh'
+                            },
+                        ]
+                    },
+                ]
+            }
+        },
+        methods:{
+            addCard:function(){
+                console.log(this);
+                swal({
+                    text: 'Please Enter your task name',
+                    content: "input",
+                    button: {
+                        text: "input",
+                        closeModal: true,
+                    },
+                })
+            },
+            viewCard:function(task){
+                var $kanbanText=task.task;
+                var _action;
+                swal({
+                    title:'Task Name',
+                    text:$kanbanText,
+                    buttons:{
+                        edit:'Edit Task',
+                        delete:{
+                            text:'Delete Task',
+                            defeat:true,
+                            dangerMode: true,
+                        },
+                        cancel:'Cancel'
+                    },
+                    closeOnClickOutside :false
+                }).then(function(value){
+                    
+                    _action=value;
+                    switch(value){
+                        case 'delete':
+                           return swal({
+                            title: "Are you sure want to delete this task?",
+                            icon: "warning",
+                            buttons: true,
+                            dangerMode: true,
+                          })
+                        case 'edit':
+                          return swal({
+                              title:'Edit Task',
+                              content:{
+                                  element:'textarea',
+                                  attributes:{
+                                    value:$kanbanText.trim(),
+                                    rows:10
+                                  }
+                              }
+                        })
+                    }
+                })
+                .then(function(){
+                    if(_action==='delete'){
+                        swal('Ini action delete')
+                    }
+                    else if(_action==='edit'){
+                        swal('Ini action edit');
+                    }
+                })
+            }
         }
-        else{
-            var pageURL='/partial/'+page+'.html';
-            $.get(pageURL).done(onPageLoaded(page)).fail(onPageFail)
-        }
-    }
-
-    function init(){
-        loadPage('kanban');
-    }
-
-    init();
+    })
 
 
 })();
--- client/js/kanban.js
+++ client/js/kanban.js
@@ -1,61 +1,28 @@
-    $('.add-card-link').click(function(e){
-        e.preventDefault()
-        swal({
-            text: 'Please Enter your task name',
-            content: "input",
-            button: {
-              text: "input",
-              closeModal: true,
-            },
-          })
+(function(){
+
+    var $cardTemplete=$('#kanban-card-template');
+    var card_template=$cardTemplete.html();
+
+    Vue.component('card-content',{
+        template:card_template,
+        data:function(){
+            return {}
+        },
+        props:['title','tasks']
     });
 
-    $('.kanban-text').click(function(){
-        var $kanbanText=$(this).text();
-        var _action;
-        swal({
-            title:'Task Name',
-            text:$kanbanText,
-            buttons:{
-                edit:'Edit Task',
-                delete:{
-                    text:'Delete Task',
-                    defeat:true,
-                    dangerMode: true,
-                },
-                cancel:'Cancel'
-            },
-            closeOnClickOutside :false
-        }).then(function(value){
-            
-            _action=value;
-            switch(value){
-                case 'delete':
-                   return swal({
-                    title: "Are you sure want to delete this task?",
-                    icon: "warning",
-                    buttons: true,
-                    dangerMode: true,
-                  })
-                case 'edit':
-                  return swal({
-                      title:'Edit Task',
-                      content:{
-                          element:'textarea',
-                          attributes:{
-                            value:$kanbanText.trim(),
-                            rows:10
-                          }
-                      }
-                })
+    new Vue({
+        el:'#app',
+        data:{
+            currentView:'kanban'
+        },
+        methods:{
+            loadView:function(view){
+                this.currentView=view;
             }
-        })
-        .then(function(){
-            if(_action==='delete'){
-                swal('Ini action delete')
-            }
-            else if(_action==='edit'){
-                swal('Ini action edit');
-            }
-        })
-    })
+        }
+    });
+    
+
+})();
+
