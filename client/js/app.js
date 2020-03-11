97ebd10f515b79324c3f19d66bff721ac4b5e61c
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-03-11 17:02:07 +0700

    - Penambahan library axios untuk versi browser dan vue.js versi development - Perubahan proses update task - Perubahan pada migration create-task dan create-user     - Untuk setting FK dari user ke task dipisahkan di migration berbeda yaitu create-user-task-fk - Implementasi create, dan delete sudah berhasil - Sebagian besar inisialisasi component ditaruh di app.js

--- client/js/app.js
+++ client/js/app.js
@@ -1,18 +1,112 @@
 (function(){
     window.TOKEN=localStorage.getItem('token');
 
+    const SERVER='http://localhost:3000';
+
     Vue.component('navbar',{
         template:'#navbar',
         methods:{
         }
-    })
-
-    Vue.component('login',{
-        template:'#login-page'
     });
 
+    const headers={
+        'Content-Type':'application/json',
+        'token':TOKEN
+    };
+
+    function validateRegisterInputs(sentData){
+        if(!sentData.email){
+
+        }
+    }
+
+    function onLoginError(err){
+        console.log({err})
+        var response=err.response;
+        if(response.status===400){
+            swal('Login Failed',response.data.message,'error');
+        }
+    }
+
+    function startLogin(sentData,self){
+        
+        axios.post(SERVER+'/login',sentData)
+        .then(function(response){
+            var token=response.data.token;
+            TOKEN=token;
+            localStorage.setItem('token',token);
+            app.currentView='kanban';
+            console.log(self.$root);
+
+        })
+        .catch(onLoginError)
+    }
+
+    function loadTasks(){
+        var self=this;
+        axios({
+            method:'get',
+            headers:headers,
+            url:SERVER+'/task'
+        })
+        .then(function(response){
+           
+            var data=response.data;
+            self.listKanban=data;
+            console.log(self);
+        })
+        .catch()
+    }
+
+    Vue.component('login',{
+        template:'#login-page',
+        data:function(){
+            return {
+                email:'',
+                password:''
+            }
+        },
+        methods:{
+            login:function(){
+                var self=this;
+                startLogin({
+                    "email":self.email,
+                    "password":self.password
+                },self)
+            }
+        }
+    });
+
+
     Vue.component('register',{
-        template:'#register-page'
+        template:'#register-page',
+        data:function(){
+            return {
+                email:'',
+                password:'',
+                name:'',
+                repassword:''
+            }
+        },
+        methods:{
+            register:function(){
+                var self=this;
+                var sentData={
+                    "email":self.email,
+                    "password":self.password,
+                    "name":self.name
+                }
+
+                axios.post(SERVER+'/register',sentData)
+                .then(function(response){
+                    var data=response.data;
+                    var email=data.email;
+                    var password=sentData.password;
+                    startLogin({email,password},self);
+                })
+                .catch(onError)
+            }
+        }
     });
 
 
@@ -20,59 +114,49 @@
         template:'#kanban-page',
         data:function(){
             return {
-                listKanban:[
-                    {
-                        title:'Ini Judul Heheheh',
-                        tasks:[
-                            {
-                                id:1,
-                                task:'Hahahahaahahahah'
-                            },
-                            {
-                                id:2,
-                                task:'Heheheh HOhoohoho'
-                            },
-                            {
-                                id:3,
-                                task:'Hihihi Hahahah Heheheheh'
-                            },
-                        ]
-                    },
-                    {
-                        title:'Ini Judul Heheheh 55555',
-                        tasks:[
-                            {
-                                id:1,
-                                task:'Hahahahaahahahah'
-                            },
-                            {
-                                id:2,
-                                task:'Heheheh HOhoohoho'
-                            },
-                            {
-                                id:3,
-                                task:'Hihihi Hahahah Heheheheh'
-                            },
-                        ]
-                    },
-                ]
+                listKanban:[]
             }
         },
+        created:function(){
+           console.log(this);
+           loadTasks.call(this);
+        },
         methods:{
-            addCard:function(){
-                console.log(this);
+            addCard:function(title){
+                var self=this;
                 swal({
                     text: 'Please Enter your task name',
                     content: "input",
                     button: {
                         text: "input",
                         closeModal: true,
+                        
                     },
+                })
+                .then(function(value){
+                    if(!value)
+                        return;
+                   var url=SERVER+'/task';
+                   var sentData={task:value,type:title};
+
+                   return axios.post(url,sentData,{
+                       headers:headers
+                   })
+
+                })
+                .then(function(){
+                    self.listKanban=[];
+                    loadTasks.call(self);
+                })
+                .catch(function(err){
+                    console.log(err);
+                        swal('Mampus','Ada error','error');
                 })
             },
             viewCard:function(task){
                 var $kanbanText=task.task;
                 var _action;
+                var self=this;
                 swal({
                     title:'Task Name',
                     text:$kanbanText,
@@ -85,7 +169,8 @@
                         },
                         cancel:'Cancel'
                     },
-                    closeOnClickOutside :false
+                    closeOnClickOutside :false,
+                    closeModal:true
                 }).then(function(value){
                     
                     _action=value;
@@ -98,29 +183,71 @@
                             dangerMode: true,
                           })
                         case 'edit':
-                          return swal({
-                              title:'Edit Task',
-                              content:{
-                                  element:'textarea',
-                                  attributes:{
-                                    value:$kanbanText.trim(),
-                                    rows:10
-                                  }
-                              }
-                        })
+                            return swal({
+                                title: 'Demo Form',
+                                content: 'input',
+                                button: {
+                                  text: 'Submit',
+                                  closeModal: false
+                                }
+                            })
                     }
                 })
                 .then(function(){
                     if(_action==='delete'){
-                        swal('Ini action delete')
+                        var url=SERVER+'/task/'+task.id;
+                        return axios({
+                            method:'DELETE',
+                            headers:headers,
+                            url:url
+                        })
+                        .then(function(){
+                            self.listKanban=[];
+                            loadTasks.call(self);
+                        })
                     }
-                    else if(_action==='edit'){
-                        swal('Ini action edit');
-                    }
+                })
+                .catch(function(err){
+                    console.log({err});
+                    swal('Mampus','Ada error','error');
                 })
             }
         }
-    })
+    });
+
+    var $cardTemplete=$('#kanban-card-template');
+    var card_template=$cardTemplete.html();
+
+    Vue.component('card-content',{
+        template:card_template,
+        data:function(){
+            return {}
+        },
+        props:['title','tasks']
+    });
+
+   var app= new Vue({
+        el:'#app',
+        data:{
+            currentView:'login'
+        },
+        methods:{
+            loadView:function(view){
+                this.currentView=view;
+            },
+            logout:function(){
+                localStorage.removeItem('token');
+                this.currentView='login';
+            }
+        }
+    });
+    
+    if(!TOKEN){
+        app.currentView='login';
+    }
+    else{
+        app.currentView='kanban';
+    }
 
 
 })();
--- client/js/kanban.js
+++ client/js/kanban.js
@@ -1,28 +1,6 @@
 (function(){
 
-    var $cardTemplete=$('#kanban-card-template');
-    var card_template=$cardTemplete.html();
 
-    Vue.component('card-content',{
-        template:card_template,
-        data:function(){
-            return {}
-        },
-        props:['title','tasks']
-    });
-
-    new Vue({
-        el:'#app',
-        data:{
-            currentView:'login'
-        },
-        methods:{
-            loadView:function(view){
-                this.currentView=view;
-            }
-        }
-    });
-    
 
 })();
 
